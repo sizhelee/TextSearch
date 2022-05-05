879142e368d8197ccf6b9d532add137754177da5
Sizhe <lisizhe@pku.edu.cn>
2022-05-05 12:16:44 +0800

    tf-idf

--- local_server.py
+++ local_server.py
@@ -6,10 +6,24 @@ from time import sleep
 import os
 import struct
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 
 def cal_news_score(goal, title_cnt, term_cnt):
     return (goal+term_cnt*10)*(title_cnt+1)
+
+
+def cal_feat_vec(x):
+    sc = StandardScaler()
+    x_std = sc.fit_transform(x)
+    cov_x = np.cov(x_std.T)
+    eigen_val, eigen_vec = np.linalg.eig(cov_x)
+
+    pair_x = [(np.abs(eigen_val[i]), eigen_vec[:, i])
+              for i in range(len(eigen_val))]
+    pair_x.sort(key=lambda k: k[0], reverse=True)
+    return pair_x[0][1].reshape(1000)
 
 
 class LocalServer(object):
@@ -18,6 +32,15 @@ class LocalServer(object):
         self.documents = []
         self.documentpath = "./server_files/news/"
         self.df = pd.read_csv("./data/all_news.csv")
+        self.feats = np.load(
+            "./server_files/file_feats_1000.npy", allow_pickle=True)
+        self.vocab_file = pd.read_csv("./server_files/vocab.csv")
+        self.vocab = list(self.vocab_file.iloc[:, 1])
+        self.similarity = np.load("./server_files/similarity.npy")
+        self.threshold = 0.1
+        self.numnews = 2225
+
+        print("[Server] Successfully init! feat shape: {}".format(self.feats.shape))
 
     def text_search(self, terms):
         result = []
@@ -31,10 +54,29 @@ class LocalServer(object):
                 goal += item.body.count(term)
                 title_cnt += item.title.count(term)
             score = cal_news_score(goal, title_cnt, term_cnt)
-            if score > 30:
+            if score > 20:
                 result.append((int(item.id), score))
         result = sorted(result, key=lambda x: (x[1], x[0]))
-        self.documents = [i[0] for i in result]
+
+        if len(result) > 0:
+            feats = []
+            for i in range(self.numnews):
+                if i+1 in result:
+                    feats.append(self.feats[i])
+                else:
+                    for j in result:
+                        if self.similarity[i][j[0]-1] > self.threshold:
+                            feats.append(self.feats[i])
+                            break
+            feats = np.vstack(feats)
+            print("[For DEBUG] feats shape: {}".format(feats.shape))
+            feat_vec = cal_feat_vec(feats)
+            cos_new = np.dot(self.feats, feat_vec)
+            result_new = np.argsort(-cos_new)[:10]
+
+            self.documents = [i+1 for i in result_new]
+        else:
+            self.documents = []
 
     def func_server(self, conn, terms, lock):
         lock.acquire()
